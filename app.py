849bf3e6bea9310a1a03b0a7829c037419f70e5d
sammytrindade <samaratrindaderibeiro@gmail.com>
2024-01-12 11:08:28 -0300

    :hammer: refactor: Add main function and create a except and put text into try

--- app.py
+++ app.py
@@ -1,27 +1,33 @@
 import requests
-from tkinter import *
 from bs4 import BeautifulSoup
+from tkinter import *
 
-link = "https://www.google.com/search?q=Cotacao+do+dolar+hoje"
-headers = {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
-}
-request = requests.get(link, headers=headers)
-# print(request)
-site = BeautifulSoup(request.text, "html.parser")
 
-title = site.find("title")
-print(title.get_text(), "\n")
+def main():
 
-valueDolar = site.find("span", class_="DFlfde")
+    link = "https://www.google.com/search?q=Cotacao+do+dolar+hoje"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
+    }
+    request = requests.get(link, headers=headers)
+    # print(request)
+    site = BeautifulSoup(request.text, "html.parser")
 
-print(valueDolar.getText(), "Dólar Americano na cotação de hoje:", "\n")
+    title = site.find("title")
+    print(title.get_text(), "\n")
 
-valueBrazil = site.find("span", class_="SwHCTb")
+    try:
+        valueDolar = site.find("span", class_="DFlfde")
+        valueBrazil = site.find("span", class_="SwHCTb")
 
-print("Valor com precisão: R$", valueBrazil["data-value"], sep=" ")
+        text = f"""
+                {valueDolar.getText()} Dólar Americano na cotação de hoje:
+                Valor com precisão: R${valueBrazil["data-value"]}
+                Valor arredondado: R${valueBrazil.get_text()}"""
+    except AttributeError as e:
+        text = f"Erro: {e}"
+    textExchange["text"] = text
 
-print("Valor arredondado: R$", valueBrazil.get_text(), sep=" ")
 
 window = Tk()
 window.title(title.get_text())
